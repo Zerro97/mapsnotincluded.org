2229cc8e8dce56f7c7dba2d03177f2db5c8524de
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2024-10-28 07:58:05 +0900

    feat: redueced number of test file & split them evenly

--- oniworldgen-js/scripts/generateTestData.js
+++ oniworldgen-js/scripts/generateTestData.js
@@ -18,11 +18,31 @@ function parseSeedString(seed) {
   return [section1, section2, section3, section4, section5];
 }
 
-function exportToTestData() {
+/**
+ * Takes in mongoDB export json file and parses it into test data.
+ * 
+ * @param {number} generatedSeedCount Total number of seed to be included in test fixture file
+ */
+function exportToTestData(generatedSeedCount) {
   let vanillaSeeds = []
   let spacedOutSeeds = []
+  let count = 0
 
   for(const cluster of mongoDBExport) {
+    // Stop adding seeds if seed count has been reached
+    if(count > generatedSeedCount) {
+      break
+    }
+
+    // Split seed into half/half for vanilla and spacedOut
+    if(cluster.asteroids.length == 1 && vanillaSeeds.length > (generatedSeedCount / 2)) {
+      continue
+    }
+    if(cluster.asteroids.length > 1 && spacedOutSeeds.length > (generatedSeedCount / 2)) {
+      continue
+    }
+
+    // Parse seed string into object with 5 parts of the seed
     let parsedSeed = parseSeedString(cluster.coordinate)
     let seed = {
       coordinate: {
@@ -35,19 +55,23 @@ function exportToTestData() {
       expectedTraits: []
     }
     
+    // Add world traits of all the asteroids in cluster
     for(const asteroid of cluster.asteroids) {
       seed.expectedTraits.push(...asteroid.worldTraits)
     }
     
+    // Split seed by asteroid count (single asteroid indicate base game)
     if(cluster.asteroids.length == 1) {
       vanillaSeeds.push(seed)
     } else {
       spacedOutSeeds.push(seed)
     }
+
+    count++
   }
 
   fs.writeFileSync('../test/fixtures/vanillaSeeds.json', JSON.stringify(vanillaSeeds, null, 4));
   fs.writeFileSync('../test/fixtures/spacedOutSeeds.json', JSON.stringify(spacedOutSeeds, null, 4));
 }
 
-exportToTestData()
+exportToTestData(100)
