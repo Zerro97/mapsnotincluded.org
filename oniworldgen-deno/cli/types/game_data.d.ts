9f094a945abac7e65b5d78601c997283ad860b75
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2024-11-03 00:17:10 +0900

    feat: working on adding types for oni yaml data schema

--- oniworldgen-deno/cli/types/game_data.d.ts
+++ oniworldgen-deno/cli/types/game_data.d.ts
@@ -1,5 +1,4 @@
-export interface GameData {
-  vanilla: {},
-  spacedOut: {},
-  frostyPlanet: {},
-}
+export type dlcType = "vanilla" | "spacedOut" | "frostyPlanet"
+
+export type GamePath = Record<dlcType, Record<"path", string>>;
+export type GameData = Record<dlcType, Record<string, string>>;
