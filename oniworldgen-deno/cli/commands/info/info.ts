cd5fc15846f880cbec664f47b79f7f181c511571
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2024-11-03 00:34:13 +0900

    feat: using correct type for options

--- oniworldgen-deno/cli/commands/info/info.ts
+++ oniworldgen-deno/cli/commands/info/info.ts
@@ -1,9 +1,11 @@
-import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 import { parseAllCommand } from "./action/all.ts";
 import { parseClusterCommand } from "./action/cluster.ts";
 import { parsePlacementCommand } from "./action/placement.ts";
 import { parseTraitCommand } from "./action/trait.ts";
 import { parseWorldCommand } from "./action/world.ts";
+
+const filter = new EnumType(["all", "cluster", "placement", "trait", "world"]);
 
 export const infoCommand = new Command()
   .name("parse")
@@ -12,24 +14,26 @@ export const infoCommand = new Command()
     // If no arguments or options are provided, show help
     this.showHelp();
   })
+  // For registering enum type for option
+  .type("filter", filter)
   // Options that are applicable for all sub commands
   .option(
-    "-k, --key <key:string>",
+    "-k, --key",
     "Output json containing unique set of key strings",
   )
   .option(
-    "-t, --test <test:string>",
+    "-t, --test",
     "Output json useful for testing",
   )
   .option(
-    "-l, --level <level:string>",
+    "-l, --level <level:integer>",
     "For selecting depth of yaml/json file",
   )
   .option(
-    "-c, --count <count:string>",
+    "-c, --count",
     "For counting number of matching data",
   )
   .option(
-    "-f, --filter <filter:string>",
+    "-f, --filter <name: filter>",
     "For filtering the data",
   )
--- oniworldgen-deno/cli/commands/parse/parse.ts
+++ oniworldgen-deno/cli/commands/parse/parse.ts
@@ -1,36 +1,40 @@
-import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 import { parseAllCommand } from "./action/all.ts";
 import { parseClusterCommand } from "./action/cluster.ts";
 import { parsePlacementCommand } from "./action/placement.ts";
 import { parseTraitCommand } from "./action/trait.ts";
 import { parseWorldCommand } from "./action/world.ts";
 
+const filter = new EnumType(["all", "cluster", "placement", "trait", "world"]);
+
 export const parseCommand = new Command()
   .name("parse")
-  .description("Parse either mongoDB export or oni yaml files and generate json")
+  .description("Parse either mongoDB export or oni yaml files and output on console")
   .action(function () {
     // If no arguments or options are provided, show help
     this.showHelp();
   })
+  // For registering enum type for option
+  .type("filter", filter)
   // Options that are applicable for all sub commands
   .option(
-    "-k, --key <key:string>",
+    "-k, --key",
     "Output json containing unique set of key strings",
   )
   .option(
-    "-t, --test <test:string>",
+    "-t, --test",
     "Output json useful for testing",
   )
   .option(
-    "-l, --level <level:string>",
+    "-l, --level <level:integer>",
     "For selecting depth of yaml/json file",
   )
   .option(
-    "-c, --count <count:string>",
+    "-c, --count",
     "For counting number of matching data",
   )
   .option(
-    "-f, --filter <filter:string>",
+    "-f, --filter <name: filter>",
     "For filtering the data",
   )
   .command("all", parseAllCommand)
